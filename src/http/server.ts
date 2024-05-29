ea0e98581f50c56aafd8cd1133a0ecae7df60573
fsanntiago <zerofabricio@hotmail.com>
2024-05-29 13:57:35 -0300

    feat: add deliver and cancel order routes

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,6 +2,8 @@ import { Elysia } from 'elysia'
 
 import { approveOrder } from './routes/approve-order'
 import { authenticateFromLink } from './routes/authenticate-from-link'
+import { cancelOrder } from './routes/cancel-order'
+import { deliverOrder } from './routes/deliver-order'
 import { dispatchOrder } from './routes/dispatch-order'
 import { getManagedRestaurant } from './routes/get-managed-restaurant'
 import { getOrderDetails } from './routes/get-order-details'
@@ -20,6 +22,8 @@ const app = new Elysia()
   .use(getOrderDetails)
   .use(approveOrder)
   .use(dispatchOrder)
+  .use(deliverOrder)
+  .use(cancelOrder)
   .onError(({ error, code, set }) => {
     switch (code) {
       case 'VALIDATION':
